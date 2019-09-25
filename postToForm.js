fd5f7f015afb536cd138769dbe33038c0e1fc955
melvingaye <32171644+melvingaye@users.noreply.github.com>
2019-09-25 11:33:37 -0400

    Update postToForm.js

--- postToForm.js
+++ postToForm.js
@@ -1,9 +1,11 @@
 //Used to get the vin from the url
-jQuery(document).ready(function(){
-    var urlVin = location.search.substring(1)
-    var vin = urlVin.slice(4,urlVin.length)
-    $('#form').submit(function(eventObj){
-    $(this).appendTo('<input type="hidden" name="vin" value="' + vin + '" />');
-    return true;
-    })
+jQuery(document).ready(function($){
+    var urlVin = location.search.substring(1);
+    var vin = urlVin.slice(4,urlVin.length);
+        $('<input>',{
+            type: 'text',
+            id: 'hiddenField',
+            name: 'vin',
+            value: vin
+        }).appendTo('#form').submit();
 });
