a75959afaab3dbb7d8279d78fc01ccf629ddff8f
melvingaye <32171644+melvingaye@users.noreply.github.com>
2019-09-25 12:39:36 -0400

    eventHandler for click event

--- postToForm.js
+++ postToForm.js
@@ -1,11 +1,15 @@
 //Used to get the vin from the url
 jQuery(document).ready(function($){
-    var urlVin = location.search.substring(1);
-    var vin = urlVin.slice(4,urlVin.length);
-        $('<input>',{
-            type: 'text',
-            id: 'hiddenField',
-            name: 'vin',
-            value: vin
-        }).appendTo('#form').submit();
+    $('#form').on('submit',function(eventObj){
+        eventObj.preventDefault();
+        var urlVin = location.search.substring(1);
+        var vin = urlVin.slice(4,urlVin.length);
+            $('<input>',{
+                type: 'text',
+                id: 'hiddenField',
+                name: 'vin',
+                value: vin
+            }).appendTo('#form');
+            $('[data-role="submit"]').focus().click();
+    });
 });
