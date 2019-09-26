5c402fd8bc47fc804e5b0bd332dfd9cdc21ef4c0
melvind.gaye <melvind.gaye@gmail.com>
2019-09-26 09:12:40 -0400

    hijacked the submit button

--- vinGetter.js
+++ vinGetter.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(function($){
-	// Short Text
-         var urlVin = location.search.substring(1);
-         var vin = urlVin.slice(4,urlVin.length);
-	jQuery('[data-id="57231621"] input').val(vin);
+	$('#form').on('submit', function(){
+        var urlVin = location.search.substring(1);
+        var vin = urlVin.slice(4,urlVin.length);
+        $('[data-id="57231621"] input').val(vin);
+    });
+    $('[data-role="submit"]').focus().click();
 });
 
 // //Used to get the vin from the url
